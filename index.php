1039fbf88fcaeb73dde98b8db5b2b6112f02c8a2
fizzvr <fizzvr@gmail.com>
2013-10-05 17:15:25 -0500

    añado las meta estandar y las propiedades del sitio

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!doctype html>
 <html lang="es">
 <head>
+	<!-- meta estandar -->
 	<meta charset="UTF-8">
-	<title>Pon_el_titulo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- propiedades del sitio -->
+	<meta name="description" content="El paradigma de la Programación Orientada a Objetos en PHP y el patrón de arquitectura de software MVC">
+	<meta name="keywords" content="las,palabras,clave">
+	<meta name="author" content="fizzvr">
+	<link rel="shortcut icon" href="act/ico/favicon.png">
+	<title>www/vr-php1 | Vladimir Rodríguez</title>
 </head>
 <body>
 
