d7d9e9e8d1522586ded2c97889388413edd0d1e6
fizzvr <fizzvr@gmail.com>
2013-10-05 19:29:21 -0500

    declaro variables y agrego el metodo abrir_conexion

---
+++ db_modelo_de_abstraccion.php
@@ -0,0 +1,25 @@
+<?php
+	abstract class DBModeloAbstraccion {
+		private static $db_servidor = "localhost";
+		private static $db_usuario = "root";
+		private static $db_clave = "1266";
+		protected $db_nombre = "miBaseDatos";
+		protected $consulta;
+		protected $filas = array();
+		private $conexion;
+
+		// metodos abstractos para ABM de clases que ereden
+		abstract protected function get();
+		abstract protected function set();
+		abstract protected function edit();
+		abstract protected function delete();
+
+		/*los siguientes metodos pueden definirse con exactitud
+		y no son abstractos*/
+		// Conectar con la BD
+		private function abrir_conexion() {
+			$this->conexion = new mysqli(self::$db_servidor,self::$db_usuario,
+				self::$db_clave,$this->$db_nombre);
+		}
+	}
+?>
