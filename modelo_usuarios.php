8629d94ef367f81651ae41e22a13f81356ede42a
fizzvr <fizzvr@gmail.com>
2013-10-05 20:04:11 -0500

    constructor de clase que re-define el nombre de la bd

--- modelo_usuarios.php
+++ modelo_usuarios.php
@@ -1,3 +1,15 @@
 <?php
-	requiere_once("db_modelo_de_abstraccion.php");
+	require_once("db_modelo_de_abstraccion.php");
+	class usuario extends DBModeloAbstraccion {
+		public $nombre;
+		public $apellido;
+		public $email;
+		private $clave;
+		protected $id;
+
+		function __construct() {
+			$this->db_nombre = "book_example";
+
+		}
+	}
 ?>
