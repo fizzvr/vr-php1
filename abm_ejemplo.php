29ac6947703c3499776622eaa2dfaab5f8c19ff2
fizzvr <fizzvr@gmail.com>
2013-10-08 10:05:27 -0500

    trayendo datos desde la BD

--- abm_ejemplo.php
+++ abm_ejemplo.php
@@ -1,3 +1,8 @@
 <?php
 	require_once('modelo_usuarios.php');
+	// traer datos de un usario
+	$usuario1 = new usuario();
+	$usuario1->get('user@email.com');
+	print $usuario1->nombre. ' ' . $usuario1->apellido. ' existe<br>';
+
 ?>
