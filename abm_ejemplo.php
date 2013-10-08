cd48ec185c2ce0d41f29bff8ef3800d7d533cd08
fizzvr <fizzvr@gmail.com>
2013-10-08 14:48:26 -0500

    añado usuarios validando si el email ya existe en el metodo set de modelo_usarios.php

--- abm_ejemplo.php
+++ abm_ejemplo.php
@@ -5,4 +5,18 @@
 	$usuario1->get('user@email.com');
 	print $usuario1->nombre. ' ' . $usuario1->apellido. ' existe<br>';
 
+	// creando un nuevo usuario
+	$datos_del_usuario = array (
+		'nombre'=>'VE',
+		'apellido'=>'DOS',
+		'email'=>'alw2ma222',
+		'clave'=>'1234567890'
+		);
+	$usuario2 = new usuario();
+	$usuario2->set($datos_del_usuario);
+	$usuario2->get($datos_del_usuario['email']);
+	print $usuario2->nombre.' '.$usuario2->apellido.' ha sido creado<br>';
+
+
+
 ?>
