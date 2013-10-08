1fc6be1d59be1b03048c1e24ee4b5ae79adf1b1b
fizzvr <fizzvr@gmail.com>
2013-10-08 14:50:11 -0500

    eliminado el archivo abm_ejemplo y lo incrusto en mi index.php

--- index.php
+++ index.php
@@ -15,8 +15,32 @@
 		<script src="act/js/html5shiv.js"></script>
 		<script src="act/js/respond.min.js" ></script>
 	<![endif] -->
+
+	<!-- recursos -->
+
 </head>
 <body>
+<?php
+	require_once('modelo_usuarios.php');
+	// traer datos de un usario
+	$usuario1 = new usuario();
+	$usuario1->get('user@email.com');
+	print $usuario1->nombre. ' ' . $usuario1->apellido. ' existe<br>';
 
+	// creando un nuevo usuario
+	$datos_del_usuario = array (
+		'nombre'=>'VE',
+		'apellido'=>'DOS',
+		'email'=>'alw2ma222',
+		'clave'=>'1234567890'
+		);
+	$usuario2 = new usuario();
+	$usuario2->set($datos_del_usuario);
+	$usuario2->get($datos_del_usuario['email']);
+	print $usuario2->nombre.' '.$usuario2->apellido.' ha sido creado<br>';
+
+
+
+?>
 </body>
 </html>
