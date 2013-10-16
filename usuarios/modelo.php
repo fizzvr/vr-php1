2ec14d5b872a7c2b1ed1c5a9208c3c92a684fc42
fizzvr <fizzvr@gmail.com>
2013-10-16 04:36:14 -0500

    El nombre del archivo cambia a model.php. Se agrega una emisión de mensaje por cada acción concluyente de cada uno de los métodos

--- usuarios/modelo.php
+++ usuarios/modelo.php
@@ -1,49 +1,60 @@
 <?php
-	require_once("db_modelo_de_abstraccion.php");
+	// Importamos el modelo de abtraccion de base de datos
+	require_once("../core/db_modelo_de_abstraccion.php");
 	class Usuario extends DBModeloAbstraccion {
+		// Propiedades
 		public $nombre;
 		public $apellido;
 		public $email;
 		private $clave;
 		protected $id;
-
+		// Método constructor
 		function __construct() {
 			$this->db_nombre = "book_example";
 
 		}
-
+		// Métodos Propios
+		// Traer datos de un  usuario
 		public function get($user_email='') {
-			if ($user_email != ''):
+			if ($user_email != ''){
 				$this->consulta ="Select id, nombre, apellido, email, clave from usuarios where email ='$user_email'; ";
 				$this->devolver_resultados_de_la_consulta();
-			endif;
+			}
 
-			if (count($this->filas) == 1):
-				foreach ($this->filas[0] as $propiedad=>$valor):
+			if (count($this->filas) == 1){
+				foreach ($this->filas[0] as $propiedad=>$valor){
 					$this->$propiedad = $valor;
-				endforeach;
-			endif;
+				}
+				$this->mensaje = "Usuario encontrado";
+			} else {
+				$this->mensaje ="Usuarios no encontrado";
+			}
 		}
 
+		// Crear un nuevo usuario
 		public function set($user_data=array()) {
-			if(array_key_exists('email', $user_data)):
+			if(array_key_exists('email', $user_data)){
 				$this->get($user_data['email']);
-				if($user_data['email'] != $this->email):
-					foreach ($user_data as $campo=>$valor):
+				if($user_data['email'] != $this->email){
+					foreach ($user_data as $campo=>$valor){
 						$$campo = $valor;
-					endforeach;
+					}
 					$this->consulta = "insert into usuarios (nombre,apellido,email,clave) values ('$nombre','$apellido','$email','$clave') ";
 					$this->ejecutar_consulta_simple();
-				endif;
-			endif;
+					$this->mensaje = "Usuario agregado exitosamente";
+				} else {
+					$this->mensaje = "Usuario ya existe";
+				}
+			} else {
+				$this->mensaje = "No se a agregado al usuario";
+			}
 		}
 
+		// modificar un un usuario
 		public function edit($user_data=array()) {
-
-			foreach ($user_data as $campo=>$valor):
+			foreach ($user_data as $campo=>$valor){
 				$$campo = $valor;
-			endforeach;
-
+			}
 			$this->consulta = "
 				update 	usuarios
 				set 	nombre='$nombre',
@@ -52,11 +63,14 @@
 				where 	email='$email'
 			";
 			$this->ejecutar_consulta_simple();
+			$this->mensaje ="Usuario modificado";
 		}
 
+		// eliminar un usuario
 		public function delete($user_email='') {
 			$this->consulta = "delete from usuarios where email='$user_email'";
 			$this->ejecutar_consulta_simple();
+			$this->mensaje = "Usuario eliminado";
 		}
 
 		public function __destruct() {
